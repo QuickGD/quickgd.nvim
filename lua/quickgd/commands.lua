5549b1d3776769dbc5b0c3282d40f98d0f6241c0
A-Lamia <git@lamia.io>
2023-03-09 16:13:04 +1100

    refactor(commands): move run_scene into godot_run

--- lua/quickgd/commands.lua
+++ lua/quickgd/commands.lua
@@ -18,6 +18,17 @@ local function run_scene(prompt_bufnr)
 end
 
 function M.godot_run()
+  local function run_scene(prompt_bufnr)
+    actions.select_default:replace(function()
+      actions.close(prompt_bufnr)
+      local selected = actions_state.get_selected_entry()
+      config.last_scene = selected.path
+      local command = string.format("silent! !%s %s", config.godot_path, selected.path)
+      vim.api.nvim_command(command)
+    end)
+    return true
+  end
+
   if config.telescope then
     telescope.run_func_on_selected({
       name = "TSCN",
